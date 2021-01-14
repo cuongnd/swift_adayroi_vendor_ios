fa3438b4a40ca13ecc2d2daee2aebb8b7eaf5bb0
cuongnd <nguyendinhcuong@gmail.com>
2021-01-14 11:26:13 +0700

    updare code

--- AdayroiVendor/Components/Ecommerce/Models/UnitModel.swift
+++ AdayroiVendor/Components/Ecommerce/Models/UnitModel.swift
@@ -9,45 +9,33 @@ import Foundation
 struct UnitModel: Codable {
     let _id: String
     let name: String
-    let status: Bool
-    let added_date: String
-    let added_user_id: String
-    let updated_date: String?
-    let updated_user_id: String
-    let ordering: Int
+    let user_id: String
     let alias: String
-    let description: String?
-    let default_photo: ImageModel?
-    let default_icon: ImageModel?
+    let description: String
+    let published: Int
+    let created_date: String
+    let modify_date: String
    
     
     enum CodingKeys: String, CodingKey {
        case _id = "_id"
         case name = "name"
-        case status = "status"
-        case added_date = "added_date"
-        case added_user_id = "added_user_id"
-        case updated_date = "updated_date"
-        case updated_user_id = "updated_user_id"
-        case ordering = "ordering"
+        case user_id = "user_id"
         case alias = "alias"
         case description = "description"
-        case default_photo =  "default_photo"
-        case default_icon = "default_icon"
+        case published = "published"
+        case created_date = "created_date"
+        case modify_date = "modify_date"
+       
     }
     init() {
         _id = ""
         name = ""
-        status = true
-        added_date = ""
-        added_user_id = ""
-        updated_date=""
-        updated_user_id=""
-        ordering=0
-        alias=""
-        description=""
-        default_photo=ImageModel()
-        default_icon=ImageModel()
-      
+        user_id = ""
+        alias = ""
+        description = ""
+        published=1
+        created_date=""
+        modify_date=""
     }
 }
